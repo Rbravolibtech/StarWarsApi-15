7eb0652115206cfb01fcc4eb6cb11c49f7344b6a
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-10-16 13:23:21 -0600

    added async function cleaned up code to be more readable

--- src/App.js
+++ src/App.js
@@ -7,27 +7,24 @@ import { useState } from "react";
 function App() {
 	const [movies, setMovies] = useState([]);
 
-	function fetchMoviesHandler() {
-		fetch(" https://swapi.py4e.com/api/films/")
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				const transformedMovies = data.results.map((movieData) => {
-					return {
-						id: movieData.episode_id,
-						title: movieData.title,
-						openingText: movieData.opening_crawl,
-						releaseDate: movieData.release_date,
-					};
-				});
-				setMovies(transformedMovies);
-			});
+	async function fetchMoviesHandler() {
+		const response = await fetch(" https://swapi.py4e.com/api/films/");
+		const data = await response.json();
+
+		const transformedMovies = data.results.map((movieData) => {
+			return {
+				id: movieData.episode_id,
+				title: movieData.title,
+				openingText: movieData.opening_crawl,
+				releaseDate: movieData.release_date,
+			};
+		});
+		setMovies(transformedMovies);
 	}
 	return (
 		<React.Fragment>
 			<section>
-				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
+				<button onClick={fetchMoviesHandler}>GET MOVIES</button>
 			</section>
 			<section>
 				<MoviesList movies={movies} />
