3273580b6ae30b1e8562b95b198d34d0444a35a8
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-10-16 14:55:23 -0600

    added styling and added code to app.js

--- src/App.js
+++ src/App.js
@@ -6,28 +6,48 @@ import { useState } from "react";
 
 function App() {
 	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
 
 	async function fetchMoviesHandler() {
-		const response = await fetch(" https://swapi.py4e.com/api/films/");
-		const data = await response.json();
+		setIsLoading(true);
+		setError(null);
+		try {
+			const response = await fetch("https://swapi.py4e.com/api/films/");
+			if (!response.ok) {
+				throw new Error(
+					"COULD NOT LOAD MOVIES CHECK YOUR INTERNET CONNECTION!!!",
+				);
+			}
+			const data = await response.json();
 
-		const transformedMovies = data.results.map((movieData) => {
-			return {
-				id: movieData.episode_id,
-				title: movieData.title,
-				openingText: movieData.opening_crawl,
-				releaseDate: movieData.release_date,
-			};
-		});
-		setMovies(transformedMovies);
+			const transformedMovies = data.results.map((movieData) => {
+				return {
+					id: movieData.episode_id,
+					title: movieData.title,
+					openingText: movieData.opening_crawl,
+					releaseDate: movieData.release_date,
+				};
+			});
+			setMovies(transformedMovies);
+		} catch (error) {
+			setError(error.message);
+		}
+		setIsLoading(false);
 	}
+
 	return (
 		<React.Fragment>
 			<section>
-				<button onClick={fetchMoviesHandler}>GET MOVIES</button>
+				<button onClick={fetchMoviesHandler}>SEARCH MOVIES</button>
 			</section>
 			<section>
-				<MoviesList movies={movies} />
+				{!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+				{!isLoading && movies.length === 0 && !console.error && (
+					<p>FOUND NO MOVIES </p>
+				)}
+				{!isLoading && error && <p>{error}</p>}
+				{isLoading && <p className="page">LOADING... LOADING... LOADING...</p>}
 			</section>
 		</React.Fragment>
 	);
